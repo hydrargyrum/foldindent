7daa5aa4d65c77992c6055b1f122fa025b4a2f48
Hg <dev@indigo.re>
2023-08-29 12:58:35 +0200

    bump to 0.3.0

--- foldindent.py
+++ foldindent.py
@@ -11,7 +11,7 @@ from textual.keys import Keys
 from textual.widgets import Footer, Tree as _Tree
 from textual.widgets.tree import TreeNode
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 @dataclass
